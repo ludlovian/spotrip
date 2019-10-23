5f5673d847a54d0c1452bde572cfdbfc42ba312e
Alan Layng <alan@readersludlow.com>
2019-10-23 10:41:11 +0100

    0.3.3

--- dist/index.js
+++ dist/index.js
@@ -314,7 +314,7 @@ class Sade {
 }
 var lib$1 = (str, isOne) => new Sade(str, isOne);
 
-var version = "0.3.2";
+var version = "0.3.3";
 
 const { FORCE_COLOR, NODE_DISABLE_COLORS, TERM } = process.env;
 const $ = {
