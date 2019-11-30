f6039850386a8e87b8fd99c53db2f984fdd80888
Alan Layng <alan@readersludlow.com>
2019-11-30 07:41:19 +0000

    1.0.3

--- dist/index.js
+++ dist/index.js
@@ -318,7 +318,7 @@ class Sade {
 }
 var lib$1 = (str, isOne) => new Sade(str, isOne);
 
-var version = "1.0.2";
+var version = "1.0.3";
 
 var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
 
