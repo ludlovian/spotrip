0f24f0990637b945805e083def2dc649a17be018
Alan Layng <alan@readersludlow.com>
2021-04-19 15:30:00 +0100

    1.5.1

--- dist/index.js
+++ dist/index.js
@@ -335,7 +335,7 @@ class Sade {
 }
 var lib$1 = (str, isOne) => new Sade(str, isOne);
 
-var version = "1.5.0";
+var version = "1.5.1";
 
 function retry (fn, opts = {}) {
   return tryOne({ ...opts, fn, attempt: 1 })
@@ -1019,16 +1019,57 @@ function plural(ms, msAbs, n, name) {
 const CSI = '\u001B[';
 const CR = '\r';
 const EOL = `${CSI}0K`;
-function log (string) {
-  log.status(string + '\n');
-  log.dirty = false;
+function log (string, { newline = true, limitWidth } = {}) {
+  if (log.prefix) {
+    string = log.prefix + string;
+  }
+  if (limitWidth && log.width) {
+    string = truncateToWidth(string, log.width);
+  }
+  if (log.dirty) {
+    string = CR + EOL + string;
+  }
+  if (newline) {
+    string = string + '\n';
+    log.dirty = false;
+  } else {
+    log.dirty = true;
+  }
+  process.stdout.write(string);
 }
-log.status = string => {
-  const clear = log.dirty ? CR + EOL : '';
-  process.stdout.write(`${clear}${log.prefix}${string}`);
-  log.dirty = true;
-};
+log.status = string =>
+  log(string, {
+    newline: false,
+    limitWidth: true
+  });
 log.prefix = '';
+if (process.stdout.isTTY) {
+  log.width = process.stdout.columns;
+  process.stdout.on('resize', () => {
+    log.width = process.stdout.columns;
+  });
+}
+function truncateToWidth (string, maxWidth) {
+  if (string.length < maxWidth) return string
+  const parts = [];
+  let w = 0;
+  let full;
+  for (const match of string.matchAll(RE_DECOLOR)) {
+    const [, text, ansiCode] = match;
+    if (full) {
+      parts.push(ansiCode);
+      continue
+    } else if (w + text.length < maxWidth) {
+      parts.push(text, ansiCode);
+      w += text.length;
+    } else {
+      parts.push(text.slice(0, maxWidth - w - 1), ansiCode);
+      full = true;
+    }
+  }
+  return parts.join('')
+}
+const RE_DECOLOR = /(^|[^\x1b]+)((?:\x1b\[\d*m)|$)/g;
 
 const { green, cyan } = kleur;
 const reporter = new EventEmitter();
