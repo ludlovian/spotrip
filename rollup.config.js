b8b569cb48d6f8ecba51ddfcb20f861a8834725d
Alan Layng <alan@readersludlow.com>
2021-04-23 14:12:38 +0100

    removed rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,6 @@ import commonjs from '@rollup/plugin-commonjs'
 export default {
   input: 'src/index.mjs',
   external: [
-    'http',
-    'fs',
-    'stream',
-    'child_process',
-    'util',
-    'events',
     'fs/promises',
     'stream/promises'
   ],
